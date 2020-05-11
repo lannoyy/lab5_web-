99218c381acb003d7913ba33cc4272426327178b
yura <ledovskij.yura@gmail.com>
2020-05-11 16:07:39 -0400

    Fix net.py and add some features

--- net.py
+++ net.py
@@ -22,14 +22,14 @@ visible2 = Input(shape=(nh,nw,ncol),name ='imginp')
 resnet = keras.applications.resnet_v2.ResNet50V2(include_top=True,weights='imagenet', input_tensor=visible2,input_shape=None, pooling=None, classes=1000)# чтение изображений из каталога# учтите если там есть файлы не соответствующие изображениям или каталоги# возникнет ошибка
 
 def read_image_files(files_max_count,dir_name):
-    files = os.listdir(dir_name)
+    files = [item.name for item in os.scandir(dir_name) if item.is_file()]
     files_count = files_max_count
     if(files_max_count>len(files)): # определяем количество файлов не больше max
         files_count = len(files)
     image_box = [[]]*files_count
     for file_i in range(files_count): # читаем изображения в список
         image_box[file_i] = Image.open(dir_name+'/'+files[file_i]) # / ??
-    return files_count, image_box# возвращаем результаты работы нейронной сети
+    return files_count, image_box
 
 
 def getresult(image_box):
--- st.sh
+++ st.sh
@@ -1,9 +1,10 @@
 gunicorn --bind 127.0.0.1:5000 wsgi:app & APP_PID=$!
-sleep 5
+sleep 25
 echo start client
 python3 client.py
+APP_CODE=$?
 sleep 5
 echo $APP_PID
 kill -TERM $APP_PID
-sleep 1
-exit 0
+echo app code $APP_CODE
+exit $APP_CODE
